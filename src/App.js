5d62585cbdc3eaf1c02a0da42e69a1632d7fc99a
Ziye <51488705+ZZtheNoob@users.noreply.github.com>
2021-06-30 20:12:11 -0400

    framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 import HomePage from "./components/homepage";
 import Working from "./components/working";
 import Project from "./components/project";
 import Education from "./components/education";
 
+import floor from "./image/hill&floor.png";
+import point from "./image/points-at-top.gif";
+
 function App() {
   return (
     <div className="App">
-      <Router>
-        <div className="mainImage"></div>
+      <img className="point" src={point} alt="point"></img>
 
+      <Router>
         <Route path="/" exact component={HomePage} />
         <Route path="/working" component={Working} />
         <Route path="/project" component={Project} />
         <Route path="/education" component={Education} />
       </Router>
+
+      <img className="floor" src={floor} alt="floor"></img>
     </div>
   );
 }
--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,9 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./homepage.css";
 
+import board from "../image/board.png";
+
 function HomePage() {
+  function jump() {
+    var character = document.getElementById("character");
+    if (character.classList === "jump-animate") {
+      return;
+    }
+    character.classList.add("jump-animate");
+    setTimeout(function () {
+      character.classList.remove("jump-animate");
+    }, 1000);
+  }
+
   return (
     <div className="container">
+      <img className="board" src={board} alt="board"></img>
+
       <div className="nav">
         <Link to="/working" className="nav-link">
           <button>Working</button>
@@ -16,7 +31,9 @@ function HomePage() {
           <button>Education</button>
         </Link>
       </div>
-      <div className="game"></div>
+
+      <div id="character"></div>
+      <button onClick={jump}>jump</button>
     </div>
   );
 }
