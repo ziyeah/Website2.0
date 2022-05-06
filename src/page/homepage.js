411d24f413c7522cf5e6cbd6a222486ec9210420
Ziye <51488705+ZZtheNoob@users.noreply.github.com>
2022-05-06 16:23:19 -0700

    homepage design update

--- src/page/homepage.js
+++ src/page/homepage.js
@@ -45,39 +45,40 @@ function HomePage() {
   return (
     <div className="container">
       <img className="board" src={board} alt="board"></img>
+      <div className="bottom-content">
+        <div className="block-popup-container">
+          <img id="mushroom" src={mushroom} alt="mushroom"></img>
+          <img id="mushroom2" src={mushroom2} alt="mushroom2"></img>
+          <img id="flower" src={flower} alt="flower"></img>
+        </div>
 
-      <div className="block-popup-container">
-        <img id="mushroom" src={mushroom} alt="mushroom"></img>
-        <img id="mushroom2" src={mushroom2} alt="mushroom2"></img>
-        <img id="flower" src={flower} alt="flower"></img>
-      </div>
-
-      <div className="block-container">
-        <img
-          className="block-clickable"
-          src={blockQuestionMark}
-          onClick={navWork}
-          alt="block-question-mark"
-        ></img>
-        <img className="block" src={block} alt="block"></img>
-        <img
-          className="block-clickable"
-          src={blockQuestionMark}
-          onClick={navProject}
-          alt="block-question-mark"
-        ></img>
-        <img className="block" src={block} alt="block"></img>
-        <img
-          className="block-clickable"
-          src={blockQuestionMark}
-          onClick={navEducation}
-          alt="block-question-mark"
-        ></img>
-      </div>
-      <div className="block-name-container">
-        <span className="block-name">work</span>
-        <span className="block-name">project</span>
-        <span className="block-name">education</span>
+        <div className="block-container">
+          <img
+            className="block-clickable"
+            src={blockQuestionMark}
+            onClick={navWork}
+            alt="block-question-mark"
+          ></img>
+          <img className="block" src={block} alt="block"></img>
+          <img
+            className="block-clickable"
+            src={blockQuestionMark}
+            onClick={navProject}
+            alt="block-question-mark"
+          ></img>
+          <img className="block" src={block} alt="block"></img>
+          <img
+            className="block-clickable"
+            src={blockQuestionMark}
+            onClick={navEducation}
+            alt="block-question-mark"
+          ></img>
+        </div>
+        <div className="block-name-container">
+          <span className="block-name" onClick={navWork}>career</span>
+          <span className="block-name" onClick={navProject}>project</span>
+          <span className="block-name-right" onClick={navEducation}>education</span>
+        </div>
       </div>
     </div>
   );
